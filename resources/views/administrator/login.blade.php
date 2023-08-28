e84c8703a5574f1004afaf3d44d4c233406d222c
souvik <souvikraybarman@gmail.com>
2023-08-29 00:31:38 +0530

    login page url to asset

--- resources/views/administrator/login.blade.php
+++ resources/views/administrator/login.blade.php
@@ -7,26 +7,26 @@
     <title>Administrator</title>
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{url('plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="{{url('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
     <!-- iCheck -->
-    <link rel="stylesheet" href="{{url('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="{{url('plugins/jqvmap/jqvmap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/jqvmap/jqvmap.min.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{url('dist/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{url('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{url('plugins/daterangepicker/daterangepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/daterangepicker/daterangepicker.css')}}">
     <!-- summernote -->
-    <link rel="stylesheet" href="{{url('plugins/summernote/summernote-bs4.min.css')}}">
-    <link rel="stylesheet" href="{{url('dist/css/theme.css')}}">
-    <link rel="stylesheet" href="{{url('plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')}}">
-    <link rel="stylesheet" href="{{url('plugins/toastr/toastr.min.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/theme.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/toastr/toastr.min.css')}}">
 
-    <script src="{{url('plugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
     <style>
     .error {
         color: #d30000;
@@ -112,34 +112,34 @@
 <!-- ./wrapper -->
 
 <!-- jQuery UI 1.11.4 -->
-<script src="{{url('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script src="{{asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
 $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="{{url('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- ChartJS -->
-<script src="{{url('plugins/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('plugins/chart.js/Chart.min.js')}}"></script>
 <!-- Sparkline -->
-<script src="{{url('plugins/sparklines/sparkline.js')}}"></script>
+<script src="{{asset('plugins/sparklines/sparkline.js')}}"></script>
 <!-- jQuery Knob Chart -->
-<script src="{{url('plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<script src="{{asset('plugins/jquery-knob/jquery.knob.min.js')}}"></script>
 <!-- daterangepicker -->
-<script src="{{url('plugins/moment/moment.min.js')}}"></script>
-<script src="{{url('plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{asset('plugins/moment/moment.min.js')}}"></script>
+<script src="{{asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{url('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<script src="{{asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
 <!-- Summernote -->
-<script src="{{url('plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="{{asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
 <!-- overlayScrollbars -->
-<script src="{{url('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{url('dist/js/adminlte.js')}}"></script>
-<script src="{{url('dist/js/pages/dashboard.js')}}"></script>
-<script src="{{url('plugins/sweetalert2/sweetalert2.min.js')}}"></script>
-<script src="{{url('plugins/toastr/toastr.min.js')}}"></script>
-<script src="{{url('dist/js/theme.js')}}"></script>
+<script src="{{asset('dist/js/adminlte.js')}}"></script>
+<script src="{{asset('dist/js/pages/dashboard.js')}}"></script>
+<script src="{{asset('plugins/sweetalert2/sweetalert2.min.js')}}"></script>
+<script src="{{asset('plugins/toastr/toastr.min.js')}}"></script>
+<script src="{{asset('dist/js/theme.js')}}"></script>
 </body>
 
 </html>
